b233c96081439c7d08d8eb0a5a983f7dacaf50a3
gultavo <gusborsatto@gmail.com>
2024-12-23 17:56:52 -0300

    javascript

---
+++ script/script.js
@@ -0,0 +1,7 @@
+situacao = {
+    "a1": false, "a2": false, "a3": false, "a4": false, "a5": false, "a6": false, "a7": false, "a8": false, "a9": false,
+    "b1": false, "b2": false, "b3": false, "b4": false, "b5": false, "b6": false, "b7": false, "b8": false, "b9": false,
+    "c1": false, "c2": false, "c3": false, "c4": false, "c5": false, "c6": false, "c7": false, "c8": false, "c9": false,
+    "d1": false, "d2": false, "d3": false, "d4": false, "d5": false, "d6": false, "d7": false, "d8": false, "d9": false,
+    "e1": false, "e2": false, "e3": false, "e4": false
+}
