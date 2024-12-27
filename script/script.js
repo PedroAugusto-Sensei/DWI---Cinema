7daaeb6d4994f225faf42c5cc9babcf061af4f68
gultavo <gusborsatto@gmail.com>
2024-12-27 16:05:24 -0300

    localStorage fixed

--- script/script.js
+++ script/script.js
@@ -12,6 +12,11 @@ function inicializador() {
     }
 }
 
+if (localStorage.length == 0) {
+    inicializador();
+}
+
+
 const vars = ["polA1", "polA2", "polA3", "polA4", "polA5", "polA6", "polB1", "polB2", "polB3", "polB4", "polB5", "polB6",
     "polC1", "polC2", "polC3", "polC4", "polC5", "polC6", "polD1", "polD2", "polD3", "polD4", "polD5", "polD6", "polE1",
     "polE2", "polE3", "polE4", "polE5", "polE6", "polF1", "polF2", "polF3", "polF4", "polF5", "polF6", "polG1", "polG2",
